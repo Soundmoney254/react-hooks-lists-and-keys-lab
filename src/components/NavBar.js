dd723a7ea6360f604f76f387c13a5113075438e3
Samuel <samnmbuguah@gmail.com>
2023-04-28 16:14:14 +0300

    COmpleted Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { v4 as uuidv4 } from 'uuid';
+
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link) => (
+        <a href={`#${link}`} key={uuidv4()} >{link}</a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, about, technologies, id }) {
   return (
-    <div className="project-item">
+    <div className="project-item" key={id}>
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+      {technologies.map((technology) => (
+          <span key={uuidv4()}>{technology}</span>
+        ))}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
